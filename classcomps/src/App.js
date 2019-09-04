e055a73a1e7afb746dcdac20ddc7a3cbf612387e
roman-jordan <rjordan1352@gmail.com>
2019-09-03 18:43:37 -0700

    Base app with Sass

---
+++ classcomps/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+export default class App extends React.Component{
+  state = {
+    todos:[]
+  }
+
+  render(){
+    return (
+      <div className="TodoContainer">
+
+      </div>
+    )
+  }
+}
